5f5fa5cff4ec61c69d08bc60cc05dfc1e7d45f57
Aravind Chinthala <aravindchinthla@gmail.com>
2023-03-29 00:06:35 +0530

    push reactapp

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+import {Box} from './Box.js'
+function App() {
+  return (
+    <div>
+    <div >
+       <Box no="1" /><Box no="2" />
+    </div>
+    <div>
+        <Box no="3" /><Box no="4" />
+    </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Box.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import './box.css'
+export const Box=(props)=>{
+    return <div className='box'>{props.no}</div>
+}
